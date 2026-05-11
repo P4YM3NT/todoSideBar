07240e1729b8c6c776d89c1adfcb7eb321a80261
P4YM3NT <johnzekiri1@gmail.com>
2026-05-11 15:04:01 +0200

    fix(electron): bundle packaged runtime dependencies

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,15 +1,19 @@
 import { resolve } from 'path';
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import { defineConfig } from 'electron-vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	main: {
-		plugins: [externalizeDepsPlugin()],
 		resolve: { alias: { '@shared': resolve('src/shared') } },
+		build: {
+			rollupOptions: { external: ['electron'] },
+		},
 	},
 	preload: {
-		plugins: [externalizeDepsPlugin()],
 		resolve: { alias: { '@shared': resolve('src/shared') } },
+		build: {
+			rollupOptions: { external: ['electron'] },
+		},
 	},
 	renderer: {
 		plugins: [react()],
